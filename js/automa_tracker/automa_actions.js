02dae99b6c20ad64667b82b5dce6802f2b67539d
Noah Bolohan <noah.bolohan@gmail.com>
2025-04-14 13:30:34 -0400

    Cubes on nectar scoring modal

--- js/automa_tracker/automa_actions.js
+++ js/automa_tracker/automa_actions.js
@@ -564,12 +564,28 @@ function check_automa_end_of_round_nectar(n_check) {
             if (`round_${nectar_spot}` in increment_card) {
                 if (increment_card[`round_${nectar_spot}`]["secondary_action"] == "place_end-of-round_cube") {
 
-                    $(`#col_automa_nectar_${nectar_spot}_count_end_of_round_increment_${n_check}`).text("+1");
+                    $("<img>").attr(
+                        {
+                            "src" : encodeURI("https://raw.githubusercontent.com/NoahBolohan/wingspan-utilities/refs/heads/main/static/misc_images/cube.webp"),
+                            "style" : "height:5vh;"
+                        }
+                    ).appendTo(
+                        $(`#col_automa_nectar_${nectar_spot}_count_end_of_round_increment_${n_check}`)
+                    );
+
                     incrementer[nectar_spot] += 1;
                 }
                 else if (increment_card[`round_${nectar_spot}`]["secondary_action"] == "remove_end-of-round_cube") {
     
-                    $(`#col_automa_nectar_${nectar_spot}_count_end_of_round_increment_${n_check}`).text("-1");
+                    $("<img>").attr(
+                        {
+                            "src" : encodeURI("https://raw.githubusercontent.com/NoahBolohan/wingspan-utilities/refs/heads/main/static/misc_images/remove-cube.png"),
+                            "style" : "height:5vh;"
+                        }
+                    ).appendTo(
+                        $(`#col_automa_nectar_${nectar_spot}_count_end_of_round_increment_${n_check}`)
+                    );
+
                     incrementer[nectar_spot] -= 1;
                 } else {
 
