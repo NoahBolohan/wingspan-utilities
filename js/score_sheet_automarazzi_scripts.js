40a2f4772887958dcf08aeddd1c70f86db5f5827
Noah Bolohan <noah.bolohan@gmail.com>
2025-11-04 11:40:58 -0500

    N player modal opens and closes

--- js/score_sheet_automarazzi_scripts.js
+++ js/score_sheet_automarazzi_scripts.js
@@ -31,6 +31,18 @@ $(document).on(
     }
 );
 
+// Custom show div
+function show_height_hidden(div_id) {
+    $(div_id).css(
+        "visibility",
+        "visible"
+    );
+    $(div_id).css(
+        "max-height",
+        "100%"
+    );
+}
+
 // Show nectar row
 $(document).ready(
 
@@ -155,39 +167,664 @@ $(document).ready(
     }
 )
 
+// Create n_players columns
+$(document).ready(
+    function() {
+
+        $.each(
+            Array.from(Array(6), (e,i)=>i+2),
+            function(key,value) {
+                $(`#col_button_${value}_players`).on(
+                    "click",
+                    function() {
+
+                        var width_p = 28;
+
+                        var width_player_col = (100-width_p)/value;
+
+                        $("#row_score_sheet").data(
+                            "n_players",
+                            value
+                        );
+                        reset_score_sheet();
+                        generate_row_headers(value, width_p, width_player_col);
+                        generate_n_score_columns(value, width_player_col);
+                        $(`#modal_n_players`).modal("hide");
+                        $("#col_oceania_expansion_checkbox").trigger("change");
+                        $("#col_duet_mode_checkbox").trigger("change");
+                        show_height_hidden("#row_score_sheet");
+                    }
+                );
+            }
+        )
+    }
+)
+
+function reset_score_sheet() {
+
+    $("#score_sheet_colgroup").empty();
+    $("#score_sheet_thead").empty();
+    $("#score_sheet_tbody").empty();
+}
+
+function generate_row_headers(n_players, width_p, width_player_col) {
+
+    $("<col>").attr(
+        {
+            style : "width: 10%;"
+        }
+    ).appendTo("#score_sheet_colgroup");
+
+    $("<col>").attr(
+        {
+            style : `width:${width_p}%;`
+        }
+    ).appendTo("#score_sheet_colgroup")
+
+    for (var i=1; i <= n_players; i++) {
+        $("<col>").attr(
+            {
+                style : `width:${width_player_col}%;`
+            }
+        ).appendTo("#score_sheet_colgroup")
+    }
+
+    // Score sheet HTML thead
+    $("<tr>").attr(
+        {
+            style : "height:11vh;",
+            id : "row_player_names"
+        }
+    ).appendTo("#score_sheet_thead");
+
+    // Score sheet HTML tbody
+    $("<tr>").attr(
+        {
+            style : "height:5vh;",
+            id : "row_birds"
+        }
+    ).appendTo("#score_sheet_tbody");
+
+    $("<tr>").attr(
+        {
+            style : "height:5vh;",
+            id : "row_bonus_cards"
+        }
+    ).appendTo("#score_sheet_tbody");
+
+    $("<tr>").attr(
+        {
+            style : "height:5vh;",
+            id : "row_end-of-round_goals"
+        }
+    ).appendTo("#score_sheet_tbody");
+
+    $("<tr>").attr(
+        {
+            style : "height:5vh;",
+            id : "row_eggs"
+        }
+    ).appendTo("#score_sheet_tbody");
+
+    $("<tr>").attr(
+        {
+            style : "height:5vh;",
+            id : "row_food_on_cards"
+        }
+    ).appendTo("#score_sheet_tbody");
+
+    $("<tr>").attr(
+        {
+            style : "height:5vh;",
+            id : "row_tucked_cards"
+        }
+    ).appendTo("#score_sheet_tbody");
+
+    $("<tr>").attr(
+        {
+            style : "visibility:collapse;height:5vh;",
+            id : "row_duet_tokens"
+        }
+    ).appendTo("#score_sheet_tbody");
+
+    $("<tr>").attr(
+        {
+            style : "visibility:collapse;height:5vh;",
+            id : "row_nectar"
+        }
+    ).appendTo("#score_sheet_tbody");
+
+    $("<tr>").attr(
+        {
+            style : "height:5vh;",
+            id : "row_total"
+        }
+    ).appendTo("#score_sheet_tbody");
+
+    // Player names + change n players button
+    var cell = $("<td>").attr(
+        {
+            style : `width:${width_p + 10}%; border-left: 0px; vertical-align: middle; text-align: center;`,
+            colspan : "2",
+            class : "cell-info"
+        }
+    )
+
+    cell.appendTo("#row_player_names");
+
+    $("#button_change_n_players").on(
+        "click",
+        function () {
+            $(`#modal_n_players`).modal("show");
+        }
+    )
+
+    // Vertical text: Amount on cards
+    var cell_vert_amount_on_cards = $("<td>").attr(
+        {
+            class : "cell-vertical",
+            rowspan : "3"
+        }
+    )
+
+    $("<div>").attr(
+        {
+            class:"rowspan-vertical"
+        }  
+    ).text(
+        "Amount on cards"
+    ).appendTo(cell_vert_amount_on_cards);
+    
+    
+    cell_vert_amount_on_cards.appendTo("#row_birds");
+
+    // Birds
+    $("<th>").attr(
+        {
+            style : `width:${width_p}%`,
+            scope : "row",
+            class : "cell-info score_sheet_cell_no_padding"
+        }
+    ).text(
+        "Birds"
+    ).appendTo("#row_birds");
+
+    // Bonus cards
+    $("<th>").attr(
+        {
+            style : `width:${width_p}%`,
+            scope : "row",
+            class : "cell-info score_sheet_cell_no_padding"
+        }
+    ).text(
+        "Bonus cards"
+    ).appendTo("#row_bonus_cards");
+
+    // End-of-round goals
+    $("<th>").attr(
+        {
+            style : `width:${width_p}%`,
+            scope : "row",
+            class : "cell-info score_sheet_cell_no_padding"
+        }
+    ).text(
+        "End-of-round goals"
+    ).appendTo("#row_end-of-round_goals");
+
+    // Vertical text: 1 point each
+    var cell_vert_1_point_each = $("<td>").attr(
+        {
+            class : "cell-vertical",
+            rowspan : "3",
+            id : "rowspan-vertical-1-pt-each"
+        }
+    )
+
+    $("<div>").attr(
+        {
+            class:"rowspan-vertical"
+        }  
+    ).text(
+        "1 point each"
+    ).appendTo(cell_vert_1_point_each);
+    
+    
+    cell_vert_1_point_each.appendTo("#row_eggs");
+
+    // Eggs
+    $("<th>").attr(
+        {
+            style : `width:${width_p}%`,
+            scope : "row",
+            class : "cell-info score_sheet_cell_no_padding"
+        }
+    ).text(
+        "Eggs"
+    ).appendTo("#row_eggs");
+
+    // Food on cards
+    $("<th>").attr(
+        {
+            style : `width:${width_p}%`,
+            scope : "row",
+            class : "cell-info score_sheet_cell_no_padding"
+        }
+    ).text(
+        "Food on cards"
+    ).appendTo("#row_food_on_cards");
+
+    // Tucked cards
+    $("<th>").attr(
+        {
+            style : `width:${width_p}%`,
+            scope : "row",
+            class : "cell-info score_sheet_cell_no_padding"
+        }
+    ).text(
+        "Tucked cards"
+    ).appendTo("#row_tucked_cards");
+
+    // Vertical text: 5/2
+    var cell_vert_52 = $("<td>").attr(
+        {
+            class : "cell-vertical",
+            rowspan : "1"
+        }
+    )
+
+    $("<div>").attr(
+        {
+            class:"rowspan-vertical"
+        }  
+    ).text(
+        "5/2"
+    ).appendTo(cell_vert_52);
+    
+    
+    cell_vert_52.appendTo("#row_nectar");
+
+    // Nectar
+    $("<th>").attr(
+        {
+            style : `width:${width_p}%`,
+            scope : "row",
+            class : "cell-info score_sheet_cell_no_padding"
+        }
+    ).text(
+        "Nectar"
+    ).appendTo("#row_nectar");
+
+    // Duet tokens in largest contiguous group
+    $("<th>").attr(
+        {
+            style : `width:${width_p}%`,
+            scope : "row",
+            class : "cell-info score_sheet_cell_no_padding"
+        }
+    ).text(
+        "Duet tokens"
+    ).appendTo("#row_duet_tokens");
+
+    // Total
+    $("<th>").attr(
+        {
+            colspan : "2",
+            class : "cell-total score_sheet_cell_no_padding"
+        }
+    ).text(
+        "Total"
+    ).appendTo("#row_total");
+}
+
+function generate_n_score_columns(n_players, width_p) {
+    for (var i=1; i <= n_players; i++) {
+
+        // Player names
+        var cell = $("<th>").attr(
+            {
+                id : `player_${i}_name`,
+                style : `width:${width_p}%`,
+                class : "cell-info"
+            }
+        ).appendTo("#row_player_names");
+        
+        var div = $("<div>").attr(
+            {
+                class : "row p-0 justify-content-center margin_auto",
+                id : `row_player_${i}_name`
+            }
+        ).appendTo(cell);
+
+        $("<input>").attr(
+            {
+                class : "bg-white col-12 input-vertical",
+                type : "text",
+                id : `input_player_${i}_name`,
+                name : `player_${i}_name`,
+                placeholder : `Player ${i}`,
+                style : "height:9vh;"
+            }
+        ).appendTo(div);
+
+        // Birds
+        var cell = $("<td>").attr(
+            {
+                id : `col_player_${i}_birds`,
+                style : `width:${width_p}%`,
+                class : "cell-input"
+            }
+        ).appendTo("#row_birds");
+        
+        $("<input>").attr(
+            {
+                type : "number",
+                id : `input_player_${i}_birds`,
+                name : `player_${i}_birds`,
+                class : "bg-white"
+            }
+        ).prop(
+            "required",
+            true
+        ).appendTo(cell);
+
+        // Bonus cards
+        var cell = $("<td>").attr(
+            {
+                id : `col_player_${i}_bonus_cards`,
+                style : `width:${width_p}%`,
+                class : "cell-input"
+            }
+        ).appendTo("#row_bonus_cards");
+        
+        $("<input>").attr(
+            {
+                type : "number",
+                id : `input_player_${i}_bonus_cards`,
+                name : `player_${i}_bonus_cards`,
+                class : "bg-white"
+            }
+        ).prop(
+            "required",
+            true
+        ).appendTo(cell);
+
+        // End-of-round goals
+        var cell = $("<td>").attr(
+            {
+                id : `col_player_${i}_end-of-round_goals`,
+                style : `width:${width_p}%`,
+                class : "cell-input"
+            }
+        ).appendTo("#row_end-of-round_goals");
+        
+        $("<input>").attr(
+            {
+                type : "number",
+                id : `input_player_${i}_end-of-round_goals`,
+                name : `player_${i}_end-of-round_goals`,
+                class : "bg-white"
+            }
+        ).prop(
+            "required",
+            true
+        ).appendTo(cell);
+
+        // Eggs
+        var cell = $("<td>").attr(
+            {
+                id : `col_player_${i}_eggs`,
+                style : `width:${width_p}%`,
+                class : "cell-input"
+            }
+        ).appendTo("#row_eggs");
+        
+        $("<input>").attr(
+            {
+                type : "number",
+                id : `input_player_${i}_eggs`,
+                name : `player_${i}_eggs`,
+                class : "bg-white"
+            }
+        ).prop(
+            "required",
+            true
+        ).appendTo(cell);
+
+        // Food on cards
+        var cell = $("<td>").attr(
+            {
+                id : `col_player_${i}_food_on_cards`,
+                style : `width:${width_p}%`,
+                class : "cell-input"
+            }
+        ).appendTo("#row_food_on_cards");
+        
+        $("<input>").attr(
+            {
+                type : "number",
+                id : `input_player_${i}_food_on_cards`,
+                name : `player_${i}_food_on_cards`,
+                class : "bg-white"
+            }
+        ).prop(
+            "required",
+            true
+        ).appendTo(cell);
+
+        // Tucked cards
+        var cell = $("<td>").attr(
+            {
+                id : `col_player_${i}_tucked_cards`,
+                style : `width:${width_p}%`,
+                class : "cell-input"
+            }
+        ).appendTo("#row_tucked_cards");
+        
+        $("<input>").attr(
+            {
+                type : "number",
+                id : `input_player_${i}_tucked_cards`,
+                name : `player_${i}_tucked_cards`,
+                class : "bg-white"
+            }
+        ).prop(
+            "required",
+            true
+        ).appendTo(cell);
+
+        // Nectar
+        var cell = $("<td>").attr(
+            {
+                id : `col_player_${i}_nectar`,
+                style : `width:${width_p}%`,
+                class : "cell-input"
+            }
+        ).appendTo("#row_nectar");
+        
+        $("<input>").attr(
+            {
+                type : "number",
+                id : `input_player_${i}_nectar`,
+                name : `player_${i}_nectar`,
+                class : "bg-white"
+            }
+        ).appendTo(cell);
+
+        // Duet tokens in largest contiguous group
+        if (i <= 2) {
+
+            var cell = $("<td>").attr(
+                {
+                    id : `col_player_${i}_duet_tokens`,
+                    style : `width:${width_p}%`,
+                    class : "cell-input"
+                }
+            ).appendTo("#row_duet_tokens");
+
+            $("<input>").attr(
+                {
+                    type : "number",
+                    id : `input_player_${i}_duet_tokens`,
+                    name : `player_${i}_duet_tokens`,
+                class : "bg-white"
+                }
+            ).appendTo(cell);
+        }
+        else {
+
+            var cell = $("<td>").attr(
+            {
+                style : `width:${width_p}%`,
+                class : "cell-disabled"
+            }
+        ).appendTo("#row_duet_tokens");
+        }
+        
+
+        // Total
+        var cell = $("<td>").attr(
+            {
+                id : `col_player_${i}_total_score`, 
+                style : `width:${width_p}%`,
+                class : "cell-total score_sheet_cell_no_padding"
+            }
+        ).appendTo("#row_total");
+        
+        $("<div>").attr(
+            {
+                type : "number",
+                id : `div_player_${i}_total_score`
+            }
+        ).appendTo(cell);
+
+        // Total to submit
+        $("<input>").attr(
+            {
+                type : "number",
+                id : `submit_player_${i}_total_score`,
+                name : `player_${i}_total_score`
+            }
+        ).appendTo($("#total_scores_to_submit"));
+
+        assign_player_event_listeners(i);
+    }
+}
+
+function assign_player_event_listeners(i) {
+
+    // Update player total score on player_birds change
+    $(`#input_player_${i}_birds`).on(
+        "change",
+        function() {
+            recompute_player_total_score(i)
+        }
+    )
+
+    // Update player total score on bonus_cards change
+    $(`#input_player_${i}_bonus_cards`).on(
+        "change",
+        function() {
+            recompute_player_total_score(i)
+        }
+    )
+
+    // Update player total score on end-of-round_goals change
+    $(`#input_player_${i}_end-of-round_goals`).on(
+        "change",
+        function() {
+            recompute_player_total_score(i)
+        }
+    )
+
+    // Update player total score on eggs change
+    $(`#input_player_${i}_eggs`).on(
+        "change",
+        function() {
+            recompute_player_total_score(i)
+        }
+    )
+
+    // Update player total score on food_on_cards change
+    $(`#input_player_${i}_food_on_cards`).on(
+        "change",
+        function() {
+            recompute_player_total_score(i)
+        }
+    )
+
+    // Update player total score on tucked_cards change
+    $(`#input_player_${i}_tucked_cards`).on(
+        "change",
+        function() {
+            recompute_player_total_score(i)
+        }
+    )
+
+    // Update player total score on nectar change
+    $(`#input_player_${i}_nectar`).on(
+        "change",
+        function() {
+            recompute_player_total_score(i)
+        }
+    )
+
+    // Update player total score on duet_tokens change
+    if (i <= 2) {
+
+        $(`#input_player_${i}_duet_tokens`).on(
+            "change",
+            function() {
+                recompute_player_total_score(i)
+            }
+        )
+    }
+}
+
+// Toggle n_players modal
+$(document).ready(
+    function () {
+
+        $('#modal_n_players').modal('toggle');
+    }
+);
+
 
 // Recompute player total score
-function recompute_player_total_score() {
+function recompute_player_total_score(i) {
 
     var total_score =  parseNaNOrInt(
-        $("#cell_player_birds").val()
+        $(`#input_player_${i}_birds`).val()
     ) + parseNaNOrInt(
-        $("#cell_player_bonus_cards").val()
+        $(`#input_player_${i}_bonus_cards`).val()
     ) + parseNaNOrInt(
-        $("#cell_player_end-of-round_goals").val()
+        $(`#input_player_${i}_end-of-round_goals`).val()
     )+ parseNaNOrInt(
-        $("#cell_player_eggs").val()
+        $(`#input_player_${i}_eggs`).val()
     ) + parseNaNOrInt(
-        $("#cell_player_food_on_cards").val()
+        $(`#input_player_${i}_food_on_cards`).val()
     ) + parseNaNOrInt(
-        $("#cell_player_tucked_cards").val()
+        $(`#input_player_${i}_tucked_cards`).val()
     ) + parseNaNOrInt(
-        $("#cell_player_nectar").val()
-    ) + parseNaNOrInt(
-        $("#cell_player_duet_tokens").val()
+        $(`#input_player_${i}_nectar`).val()
     );
+
+    if (i <= 2) {
+        total_score += parseNaNOrInt(
+            $(`#input_player_${i}_duet_tokens`).val()
+        );
+    }
 
     if (total_score > 0) {
 
-        $("#cell_player_total_score").text(
+        $(`#div_player_${i}_total_score`).text(
             total_score
         )
     }
     else {
-        $("#cell_player_total_score").text(
+        $(`#div_player_${i}_total_score`).text(
             ""
         )
-    }
+    }    
 }
 
 function prepopulate_data()
